8939450304e55ad5f3aacbb0b680f21723b9ae98
Dermoumi S <sdermoumi@gmail.com>
2017-04-17 07:16:57 +0100

    Hobbies section+, now with icons!

--- resume/js/main.js
+++ resume/js/main.js
@@ -19,13 +19,11 @@ window.__sdermoumi__ = window.__sdermoumi__ || (function() {
                         title: 'École Supérieure de Technologie Casablanca (ESTC)',
                         description: 'Professional Licence in Software Engineering and Advanced Network and System' +
                             ' Administration'
-                    },
-                    {
+                    }, {
                         prefix: 2016,
                         title: 'Institut Spécialisé de Technologies Appliquées Sala al-Jadida (ISTA-SAJ)',
                         description: 'Specialized Technician Diploma in Software Development'
-                    },
-                    {
+                    }, {
                         prefix: 2011,
                         title: 'Lycée Mohamed VI, Sala al-Jadida',
                         description: 'Baccalaureat in Life and Earth Sciences',
@@ -38,12 +36,10 @@ window.__sdermoumi__ = window.__sdermoumi__ || (function() {
                     {
                         prefix: 2016,
                         title: 'Microsoft® Specialist: Programming in HTML5 with JavaScript and CSS3'
-                    },
-                    {
+                    }, {
                         prefix: 2016,
                         title: 'Microsoft® Specialist: Programming in C#'
-                    },
-                    {
+                    }, {
                         prefix: 2015,
                         title: 'Microsoft® Certified Professional'
                     }
@@ -56,8 +52,7 @@ window.__sdermoumi__ = window.__sdermoumi__ || (function() {
                         prefix: 'French',
                         title: 'Fluent',
                         description: 'Able to quickly express ideas, develop views or write reports.'
-                    },
-                    {
+                    }, {
                         prefix: 'English',
                         title: 'Fluent',
                         description: 'Able to quickly express ideas, develop views, write reports or contribute to a discussion.'
@@ -66,7 +61,14 @@ window.__sdermoumi__ = window.__sdermoumi__ || (function() {
             },
             {
                 title: 'Hobbies',
-                description: 'Video games, Gamedev, Comics, Worldbuilding and Light Novels'
+                template: 'hobbies-section',
+                items: {
+                    'videogames': 'Video games',
+                    'gamedev': 'Gamedev',
+                    'comics': 'Comics',
+                    'worldbuilding': 'Worldbuilding',
+                    'lightnovels': 'Light Novels'
+                }
             }
         ],
         sectionsRight: [
@@ -76,28 +78,22 @@ window.__sdermoumi__ = window.__sdermoumi__ || (function() {
                     {
                         title: 'Advanced Object Oriented Programming',
                         description: 'C++11, C#, Java, JavaScript, Python...'
-                    },
-                    {
+                    }, {
                         title: 'Intermediate Mastery of database Management Systems',
                         description: 'MySQL, Microsoft® SQL Server...'
-                    },
-                    {
+                    }, {
                         title: 'Development of Desktop Applications with Graphical Interfaces',
                         description: 'Qt5, .NET Winforms, Swing...'
-                    },
-                    {
+                    }, {
                         title: 'Full-Stack Web Development',
                         description: 'CodeIgniter, Django, J2EE, Express, Vue.js...'
-                    },
-                    {
+                    }, {
                         title: 'Game Development and Hardware-Accelerated Applications',
                         description: 'OpenGL, GLES, WebGL, SFML2, Löve2D...',
-                    },
-                    {
+                    }, {
                         title: 'At ease with Command-line Tools',
                         description: 'Git, SSH, Task runners...'
-                    },
-                    {
+                    }, {
                         title: 'Agile Development',
                         description: 'XP, SCRUM, Kanban...'
                     }
@@ -112,16 +108,14 @@ window.__sdermoumi__ = window.__sdermoumi__ || (function() {
                         items: [
                             'Using Django (and Python) to build a full-fledged web application with user management and administration panel',
                         ]
-                    },
-                    {
+                    }, {
                         prefix: 2015,
                         title: 'End-of-course Intership at CACIOPÉE S.A.R.L. (Hay Riad, Rabat)',
                         items: [
                             'Making of an HR managemnet application for a governmental entity using an in-house tool (WORKS) that uses J2EE and jBOSS based technologies.',
                             'Understanding BPMN process design and implementing workflows with jBPM. Implementing business rules with DROOLS.'
                         ]
-                    },
-                    {
+                    }, {
                         prefix: 2014,
                         title: 'Personal Project: M2N (2D Multiplatform Game Engine, C++/Lua)',
                         items: [
@@ -129,8 +123,7 @@ window.__sdermoumi__ = window.__sdermoumi__ || (function() {
                             'Understanding Unicode and how to transform and render Arabic text.',
                             'Custom implementation of secure Threads on both CPU and GPU levels on a Lua virtual machine.'
                         ]
-                    },
-                    {
+                    }, {
                         prefix: 2011,
                         title: 'Personal project: Rockout (Web-based guitar hero clone, JavaScript/PHP)',
                         items: [
@@ -164,14 +157,12 @@ window.__sdermoumi__ = window.__sdermoumi__ || (function() {
                         title: 'École Supérieure de Technologie Casablanca (ESTC)',
                         description: 'Licence Professionnelle en Génie Logiciel et Administration Avancée de Systèmes et Réseaux Informatiques',
                         notes: 'Majorant du premier semestre.'
-                    },
-                    {
+                    }, {
                         prefix: 2016,
                         title: 'Institut Spécialisé de Technologies Appliquées Sala al-Jadida (ISTA-SAJ)',
                         description: 'Diplôme de Technicien Spécialisé en Développement Informatique.',
                         notes: 'Majorant de classe.'
-                    },
-                    {
+                    }, {
                         prefix: 2011,
                         title: 'Lycée Mohamed VI, Sala al-Jadida',
                         description: 'Baccalauréat en Sciences de la Vie et de la Terre',
@@ -184,12 +175,10 @@ window.__sdermoumi__ = window.__sdermoumi__ || (function() {
                     {
                         prefix: 2016,
                         title: 'Microsoft® Specialist: Programming in HTML5 with JavaScript and CSS3'
-                    },
-                    {
+                    }, {
                         prefix: 2016,
                         title: 'Microsoft® Specialist: Programming in C#'
-                    },
-                    {
+                    }, {
                         prefix: 2015,
                         title: 'Microsoft® Certified Professional'
                     }
@@ -202,8 +191,7 @@ window.__sdermoumi__ = window.__sdermoumi__ || (function() {
                         prefix: 'Français',
                         title: 'Courant',
                         description: "Capable d'exprimer des idées, développer un point de vue ou écrire des rapports."
-                    },
-                    {
+                    }, {
                         prefix: 'English',
                         title: 'Courant',
                         description: "Capable d'exprimer des idées, développer un point de vue, contribuer à une discussion ou écrire des rapports."
@@ -212,7 +200,14 @@ window.__sdermoumi__ = window.__sdermoumi__ || (function() {
             },
             {
                 title: 'Loisirs et Intérêts',
-                description: 'Jeux Vidéo, Gamedev, Bandes dessinées, Worldbuilding et Light novels'
+                template: 'hobbies-section',
+                items: {
+                    'videogames': 'Jeux Vidéo',
+                    'worldbuilding': 'Worldbuilding',
+                    'gamedev': 'Gamedev',
+                    'comics': 'Bandes Dessinées',
+                    'lightnovels': 'Light novels'
+                }
             }
         ],
         sectionsRight: [
@@ -222,28 +217,22 @@ window.__sdermoumi__ = window.__sdermoumi__ || (function() {
                     {
                         title: 'Notions avancées en Programmation Orientée Objet',
                         description: 'C++11, C#, Java, JavaScript, Python...'
-                    },
-                    {
+                    }, {
                         title: 'Maitrise intermédiaire de langages de gestion de bases de donnée',
                         description: 'MySQL, Microsoft® SQL Server...'
-                    },
-                    {
+                    }, {
                         title: "Créàtion d'applications buréau à interfaces graphiques",
                         description: 'Qt5, .NET Winforms, Swing...'
-                    },
-                    {
+                    }, {
                         title: 'Développement Web Full-Stack',
                         description: 'CodeIgniter, Django, J2EE, Express, Vue.js...'
-                    },
-                    {
+                    }, {
                         title: 'Développement de jeux-vidéo et applications avec accélération matérielle',
                         description: 'OpenGL, GLES, WebGL, SFML2, Löve2D...',
-                    },
-                    {
+                    }, {
                         title: "Confortable avec l'utilisation d'outils en ligne de commande",
                         description: 'Git, SSH, Task runners...'
-                    },
-                    {
+                    }, {
                         title: 'Notions en méthodes de développement Agile',
                         description: 'XP, SCRUM, Kanban...'
                     }
@@ -258,16 +247,14 @@ window.__sdermoumi__ = window.__sdermoumi__ || (function() {
                         items: [
                             "Utilisation du Framework Django (et Python) pour construire un site web dynamique avec gestion d'utilisateurs et panneau d'administration",
                         ]
-                    },
-                    {
+                    }, {
                         prefix: 2015,
                         title: "Stage de fin d'études : Caciopée S.A.R.L. (Hay Riad, Rabat)",
                         items: [
                             "Participation à la création d'une application de gestion RH pour une entité gouvernementale à l'aide d'un outil fait-maison créé avec J2EE et basé sur des technologies jBOSS.",
                             "Travail avec processus BPMN pour conception et implémentation de workflows d'application avec jBPM. Implémentation de règles métier avec DROOLS."
                         ]
-                    },
-                    {
+                    }, {
                         prefix: 2014,
                         title: 'Projet personnel : M2N (Moteur de jeu 2D multiplateforme, C++11/Lua)',
                         items: [
@@ -275,8 +262,7 @@ window.__sdermoumi__ = window.__sdermoumi__ || (function() {
                             "Rendu de texte arabe en Unicode avec transformation à la volée.",
                             "Implémentation personnalisée de Threads sécurisés au niveau CPU et GPU sur la machine virtuelle Lua."
                         ]
-                    },
-                    {
+                    }, {
                         prefix: 2011,
                         title: "Projet personnel : Rockout (Jeu de rythme basé sur navigateur, JavaScript/PHP)",
                         items: [
@@ -291,6 +277,11 @@ window.__sdermoumi__ = window.__sdermoumi__ || (function() {
     Vue.component('resume-section', {
         props: ['section'],
         template: '#resume-section'
+    });
+
+    Vue.component('hobbies-section', {
+        props: ['section'],
+        template: '#hobbies-section'
     });
 
     var vm = new Vue({
